5c61160aa9a975990e6b39be4ec844ebf2d48df8
Daniel Rhaidan <106990950+DaniFzn@users.noreply.github.com>
2022-09-06 16:55:21 -0300

    First Commit Update

---
+++ script.js
@@ -0,0 +1,38 @@
+let btnN = document.querySelector("#btn-no")
+let btnY = document.querySelector("#btn-yes")
+let btnNStyle = document.querySelector(".btnNStyle")
+let btnYStyle = document.querySelector(".btnYStyle")
+let buttons = document.querySelector(".buttons")
+
+btnN.addEventListener("click", showBtnY);
+
+function showBtnY(){
+
+    if(btnYStyle.style.display = "block"){
+        btnN.addEventListener("mouseenter", randomPos);
+    } else {
+        btnYStyle.style.display = "block"
+    }
+}
+
+function randomPos(){
+
+    let randomT = Math.floor(Math.random() * 300) + 1;
+    let randomB = Math.floor(Math.random() * 300) + 1;
+    let randomL = Math.floor(Math.random() * 300) + 1;
+    let randomR = Math.floor(Math.random() * 300) + 1;
+    
+    btnNStyle.style.background = "rgb(247, 81, 81)"
+    btnNStyle.style.cursor ="auto"
+    btnNStyle.style.position = "relative"
+    btnNStyle.style.top = `${randomT}px`
+    btnNStyle.style.left = `${randomL}px`
+    btnNStyle.style.bottom = `${randomB}px`
+    btnNStyle.style.right = `${randomR}px`
+}
+
+btnY.addEventListener("click", accept);
+
+function accept() {
+    buttons.style.display = "none"
+}
